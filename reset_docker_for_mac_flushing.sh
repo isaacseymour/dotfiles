622b77a82781326c9655f3129548c4d2f5f4a7d1
Isaac Seymour <i.seymour@oxon.org>
2017-03-09 11:08:47 +0000

    Add docker-for-mac flushing reset script

---
+++ reset_docker_for_mac_flushing.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Taken from https://github.com/docker/for-mac/issues/668#issuecomment-284028148
+
+set -e
+cd ~/Library/Containers/com.docker.docker/Data/database
+git reset --hard
+
+echo -n "Current full-sync-on-flush setting: "
+cat ./com.docker.driver.amd64-linux/disk/full-sync-on-flush
+echo
+
+echo -n "Current on-flush setting: "
+cat ./com.docker.driver.amd64-linux/disk/on-flush
+echo
+
+echo -n false > ./com.docker.driver.amd64-linux/disk/full-sync-on-flush
+echo -n none > ./com.docker.driver.amd64-linux/disk/on-flush
+
+git add ./com.docker.driver.amd64-linux/disk/full-sync-on-flush
+git add ./com.docker.driver.amd64-linux/disk/on-flush
+git commit -s -m "disable flushing"
+
+echo "Docker should restart by itself now."
